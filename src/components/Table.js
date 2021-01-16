1292cac8d88adf4eaf20943c4d4a9e36525a5cbe
Tobechi <tobechichukwuleta125@gmail.com>
2021-01-15 16:49:17 -0800

    another try

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,26 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import BootstrapTable from 'react-bootstrap-table-next'
-import filterFactory, { selectFilter } from 'react-bootstrap-table2-filter';
 import paginationFactory, {
     PaginationProvider,
-    SizePerPageDropdownStandalone,
     PaginationListStandalone
 } from "react-bootstrap-table2-paginator";
-import * as ReactBootstrap from 'react-bootstrap'
 import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit'
 const { SearchBar } = Search
 
 function Table() {
     const [profiles, setProfiles] = useState([])
-    const [loading, setLoading] = useState(false)
 
     const getProfile = async () => {
         try {
             const result = await axios.get('http://api.enye.tech/v1/challenge/records')
             console.log(result.data)
             setProfiles(result.data.records.profiles)
-            setLoading(true)
         }
         catch (e) {
             console.log(e)
@@ -53,8 +48,6 @@ function Table() {
             sort: true,
             headerSortingStyle,
         },
-        // { dataField: 'Latitude', text: 'Latitude' },
-        // { dataField: 'Longitude', text: 'Longitude' },
         { 
             dataField: 'PhoneNumber', 
             text: 'Phone Number',
@@ -73,16 +66,6 @@ function Table() {
             sort: true,
             headerSortingStyle
         },
-        // { dataField: 'Email', text: 'Email' },
-        // { dataField: 'DomainName', text: 'Domain Name' },
-        // { dataField: 'URL', text: 'URL' },
-        // { dataField: 'UserName', text: 'User Name' },
-        // { 
-        //     dataField: 'LastLogin', 
-        //     text: 'Last Login',
-        //     sort: true,
-        //     headerSortingStyle
-        // },
         { 
             dataField: 'PaymentMethod', 
             text: 'Payment Method',
@@ -113,31 +96,29 @@ function Table() {
                 keyField='MacAddress'
                 columns={columns}
                 data={profiles}
-                >
-
+            >
                 {({ paginationProps, paginationTableProps }) => (
                     <ToolkitProvider
                     keyField='MacAddress'
                     columns={columns}
                     data={profiles}
-                    filter={ filterFactory() }
                     search
                     >
                         {toolkitprops => (
-                <div>
-                  <SearchBar {...toolkitprops.searchProps} />
-                  <BootstrapTable
-                    {...toolkitprops.baseProps}
-                    {...paginationTableProps}
-                    defaultSorted={defaultSorted}
-                    defaultSortDirection="asc"
-                    hover
-                    condensed
-                    noDataIndication="No Data Is Available"
-                  />
-                  <PaginationListStandalone {...paginationProps} />
-                </div>
-              )}
+                            <div>
+                                <SearchBar {...toolkitprops.searchProps} />
+                                <BootstrapTable
+                                    {...toolkitprops.baseProps}
+                                    {...paginationTableProps}
+                                    defaultSorted={defaultSorted}
+                                    defaultSortDirection="asc"
+                                    hover
+                                    condensed
+                                    noDataIndication="No Data Is Available"
+                                />
+                                <PaginationListStandalone {...paginationProps} />
+                            </div>
+                        )}
                     </ToolkitProvider>
                 )}
             </PaginationProvider>
